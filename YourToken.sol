518adf79ba863e162c83a47fd97c516fd15df6bd
Uzair <67993353+uzair011@users.noreply.github.com>
2022-08-03 22:21:37 +0530

    first commit
    Only the solidity files.

---
+++ YourToken.sol
@@ -0,0 +1,15 @@
+pragma solidity 0.8.4;
+// SPDX-License-Identifier: MIT
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+// learn more: https://docs.openzeppelin.com/contracts/3.x/erc20
+
+contract YourToken is ERC20 {
+    constructor() ERC20("UZAIR'S COIN", "UZR") {
+        //_mint( ~~~YOUR FRONTEND ADDRESS HERE~~~~ , 1000 * 10 ** 18);
+        //_mint(msg.sender, initialSupply);
+
+        _mint(msg.sender, 1000 * 10**18);
+    }
+}
