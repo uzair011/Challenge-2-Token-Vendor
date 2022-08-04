b877b65337732e69059280170a2271a6512f8a0f
Uzair <67993353+uzair011@users.noreply.github.com>
2022-08-04 22:27:59 +0530

    Second commit

--- Vendor.sol
+++ Vendor.sol
@@ -1,23 +1,27 @@
-pragma solidity 0.8.4;
 // SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./YourToken.sol";
 
 contract Vendor is Ownable {
+    event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
 
-  //event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+    YourToken public yourToken;
+    uint256 public constant tokensPerEth = 100;
 
-  YourToken public yourToken;
+    constructor(address tokenAddress) {
+        yourToken = YourToken(tokenAddress);
+    }
 
-  constructor(address tokenAddress) {
-    yourToken = YourToken(tokenAddress);
-  }
+    // ToDo: create a payable buyTokens() function:
+    function buyTokens() public payable {
+        uint256 amountOfTokens = msg.value * tokensPerEth;
+        yourToken.transfer(msg.sender, amountOfTokens);
+    }
 
-  // ToDo: create a payable buyTokens() function:
+    // ToDo: create a withdraw() function that lets the owner withdraw ETH
+    function withdraw(uint256 withdrawAmount) public onlyOwner {}
 
-  // ToDo: create a withdraw() function that lets the owner withdraw ETH
-
-  // ToDo: create a sellTokens(uint256 _amount) function:
-
+    // ToDo: create a sellTokens(uint256 _amount) function:
 }
