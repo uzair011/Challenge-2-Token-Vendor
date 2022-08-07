f9c74b2bf202c37d35f4be88ab02b15c0b24900d
Uzair <67993353+uzair011@users.noreply.github.com>
2022-08-07 10:16:04 +0530

    Third commit

--- Vendor.sol
+++ Vendor.sol
@@ -6,7 +6,11 @@ import "./YourToken.sol";
 
 contract Vendor is Ownable {
     event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
-
+    event SellTokens(
+        address seller,
+        uint256 amountOfETH,
+        uint256 amountOfTokens
+    );
     YourToken public yourToken;
     uint256 public constant tokensPerEth = 100;
 
@@ -16,12 +20,34 @@ contract Vendor is Ownable {
 
     // ToDo: create a payable buyTokens() function:
     function buyTokens() public payable {
+        require(msg.value > 0, "Spend some ethers...");
         uint256 amountOfTokens = msg.value * tokensPerEth;
         yourToken.transfer(msg.sender, amountOfTokens);
+        emit BuyTokens(msg.sender, msg.value, amountOfTokens);
     }
 
     // ToDo: create a withdraw() function that lets the owner withdraw ETH
-    function withdraw(uint256 withdrawAmount) public onlyOwner {}
+    function withdraw() external onlyOwner {
+        //require(msg.sender == owner, "Only owner can withdraw the money.");
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        //  (bool callSuccess, ) = payable(msg.sender).call{
+        //     value: address(this).balance
+        // }("");
+
+        require(callSuccess, "ETH Withdraw failed!");
+    }
 
     // ToDo: create a sellTokens(uint256 _amount) function:
+    function sellTokens(uint256 _amount) external {
+        //yourToken.approve(msg.sender, _amount);
+        uint256 payingAmount = _amount / 100;
+        yourToken.transferFrom(msg.sender, address(this), _amount);
+        (bool callSuccess, ) = payable(msg.sender).call{value: payingAmount}(
+            ""
+        );
+        require(callSuccess, "Failed to sell tokens...:(");
+        emit SellTokens(msg.sender, _amount, payingAmount);
+    }
 }
