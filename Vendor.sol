1b30b73784660ecc2ce65e5d237183beb2a3d52f
Uzair <67993353+uzair011@users.noreply.github.com>
2022-08-08 22:36:46 +0530

    Fourth commit

--- Vendor.sol
+++ Vendor.sol
@@ -19,18 +19,24 @@ contract Vendor is Ownable {
     }
 
     // ToDo: create a payable buyTokens() function:
-    function buyTokens() public payable {
+    function buyTokens() public payable returns (uint256) {
+        //! first
         require(msg.value > 0, "Spend some ethers...");
-        uint256 amountOfTokens = msg.value * tokensPerEth;
-        yourToken.transfer(msg.sender, amountOfTokens);
-        emit BuyTokens(msg.sender, msg.value, amountOfTokens);
+
+        uint256 _amountOfTokens = msg.value * tokensPerEth;
+
+        yourToken.transfer(msg.sender, _amountOfTokens);
+        emit BuyTokens(msg.sender, msg.value, _amountOfTokens);
+        return _amountOfTokens;
     }
 
     // ToDo: create a withdraw() function that lets the owner withdraw ETH
-    function withdraw() external onlyOwner {
+    function withdraw() public onlyOwner {
+        //! second
+        uint256 contractOwnerBalance = address(this).balance;
         //require(msg.sender == owner, "Only owner can withdraw the money.");
         (bool callSuccess, ) = payable(msg.sender).call{
-            value: address(this).balance
+            value: contractOwnerBalance
         }("");
         //  (bool callSuccess, ) = payable(msg.sender).call{
         //     value: address(this).balance
@@ -42,11 +48,16 @@ contract Vendor is Ownable {
     // ToDo: create a sellTokens(uint256 _amount) function:
     function sellTokens(uint256 _amount) external {
         //yourToken.approve(msg.sender, _amount);
-        uint256 payingAmount = _amount / 100;
-        yourToken.transferFrom(msg.sender, address(this), _amount);
-        (bool callSuccess, ) = payable(msg.sender).call{value: payingAmount}(
-            ""
+        uint256 payingAmount = _amount / tokensPerEth;
+        bool callSuccess = yourToken.transferFrom(
+            msg.sender,
+            address(this),
+            _amount
         );
+        // (bool callSuccess, ) = payable(msg.sender).call{value: payingAmount}(
+        //     ""
+        // );
+        (callSuccess, ) = msg.sender.call{value: payingAmount}("");
         require(callSuccess, "Failed to sell tokens...:(");
         emit SellTokens(msg.sender, _amount, payingAmount);
     }
